76c7bb903e04b7f66766a33508571dfe48066c2b
Haidar Almajedi <haidar@deranged.dk>
2024-05-31 20:41:56 +0200

    cleaner map code using a package

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,11 @@
 <script>
-    import '$node-modules/mapbox-gl/dist/mapbox-gl.css';
-    import { PUBLIC_MAPBOX_ACCESS_TOKEN, PUBLIC_DO_SPACES_IMAGE_CDN } from '$env/static/public';
+    import { Map, Marker } from '@beyonk/svelte-mapbox'
+    import { onDestroy } from 'svelte';
+    import { PUBLIC_MAPBOX_ACCESS_TOKEN } from '$env/static/public';
     import { page } from '$app/stores';
     import { PropertyCard } from '$lib/components';
-    import mapbox from 'mapbox-gl';
-    import { onMount, onDestroy } from 'svelte';
 
     let map;
-    let mapContainer;
     let zoom = 12;
     //coordinates for Copenhagen
     let lon = 12.5697339;
@@ -16,49 +14,8 @@
     $: {
         lat = $page.url.searchParams?.get('lat') ?? lat;
         lon = $page.url.searchParams?.get('lon') ?? lon;
-        map?.flyTo({ center: [ lon, lat ], zoom })
+        map?.flyTo({ center: [ lon, lat ], zoom });
     }
-
-    onMount(() => {
-        map = new mapbox.Map({
-            container: mapContainer,
-            accessToken: PUBLIC_MAPBOX_ACCESS_TOKEN,
-            style: `mapbox://styles/mapbox/outdoors-v11`,
-            center: [ lon, lat ],
-            zoom,
-        });
-
-        for (const property of $page.data.properties) {
-            const marker = new mapbox.Marker()
-                .setLngLat([ property.lon, property.lat ])
-                .setPopup(
-                    new mapbox.Popup({ offset: 25, closeButton: false, maxWidth: 'none' }) // add popups
-                        .setHTML(`
-                            <a href="/properties/${property.id}">
-                                <section class="w-[16rem] h-fit min-w-[0] bg-white rounded-md drop-shadow-around-md">
-                                    <img
-                                        class="w-full aspect-[4/3] object-cover rounded-t-md"
-                                        src="${PUBLIC_DO_SPACES_IMAGE_CDN}/${property.userId}/${property.id}/1.png"
-                                        alt="${property.name}"
-                                    >
-                                    <section class="p-3">
-                                        <h3 class="text-lg font-bold text-ellipsis overflow-hidden text-nowrap mb-1">${property.name}</h3>
-                                        <p class="text-ellipsis overflow-hidden line-clamp-2 mb-4">${property.place}</p>
-                                        <p class="text-ellipsis overflow-hidden text-nowrap" slot="more-info">
-                                            <strong>${property.pricePerNight} DKK</strong> night
-                                        </p>
-                                    </section>
-                                </section>
-                            </a>
-                        `)
-                )
-                .addTo(map);
-
-            marker.getElement().addEventListener("click", () => {
-                map.flyTo({ center: [ property.lon, property.lat ] })
-            });
-        }
-    });
 
     onDestroy(() => {
         map?.remove();
@@ -97,13 +54,35 @@
         </section>
     </div>
     <div class="hidden w-2/5 h-[calc(100%-5rem)] absolute right-0 lg:block">
-      <div class="w-full h-full" bind:this="{mapContainer}" />
+        <Map
+            accessToken={PUBLIC_MAPBOX_ACCESS_TOKEN}
+            center={[ lon, lat ]}
+            {zoom}
+            bind:map={map}
+        >
+            {#each $page.data.properties as property}
+                <Marker
+                    lat={property.lat}
+                    lng={property.lon}
+                    popupOptions={{ offset: 25, closeButton: false, maxWidth: '16rem' }}
+                    on:click={() => map?.flyTo({ center: [ property.lon, property.lat ] })}
+                >
+                    <a class="h-fit outline-none" href="/properties/{property.id}" slot="popup">
+                        <PropertyCard {property} fullWidth>
+                            <p class="text-ellipsis overflow-hidden text-nowrap" slot="more-info">
+                                <strong>{property.pricePerNight} DKK</strong> night
+                            </p>
+                        </PropertyCard>
+                    </a>
+                </Marker>
+            {/each}
+        </Map>
     </div>
 </div>
 
 <style>
     :global(.mapboxgl-popup-content) {
-        padding: 0;
-        border-radius: 0.375rem;
+        padding: 0 !important;
+        border-radius: 0.375rem !important;
     }
 </style>
