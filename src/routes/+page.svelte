94df0b330e1078818589a402cf6ba25b601330d4
Haidar Almajedi <haidar@deranged.dk>
2024-06-01 21:05:02 +0200

    only show properties in view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,10 +11,24 @@
     let lon = 12.5697339;
     let lat = 55.6753132;
 
+    let loading = true;
+    let properties = [];
+
+    $: map?.on('movestart', onMapMoveStart);
     $: {
         lat = $page.url.searchParams?.get('lat') ?? lat;
         lon = $page.url.searchParams?.get('lon') ?? lon;
         map?.flyTo({ center: [ lon, lat ], zoom });
+    }
+
+    function onMapMoveStart() {
+        loading = true;
+    }
+    function onMapMoveEnd() {
+        properties = $page.data.properties.filter((property) => {
+            return map.getBounds().contains([property.lon, property.lat]);
+        });
+        loading = false;
     }
 
     onDestroy(() => {
@@ -28,29 +42,38 @@
 
 <div class="">
     <div class="[w-full] h-[calc(100%-5rem)]  absolute overflow-y-scroll lg:[&::-webkit-scrollbar]:hidden py-8 px-4 lg:w-3/5">
-        {#if $page.data.properties.length}
-            <section class={[
-                'w-full h-fit [display:grid] [grid-template-columns:repeat(1,minmax(0,1fr))] [grid-auto-rows:auto] [grid-auto-flow:row] gap-6',
-                'sm:[grid-template-columns:repeat(2,minmax(0,1fr))]',
-                '2xl:[grid-template-columns:repeat(3,minmax(0,1fr))]',
-                '3xl:[grid-template-columns:repeat(4,minmax(0,1fr))]',
-            ].join(' ')}>
-                    {#each $page.data.properties as property}
-                        <a class="h-fit" href="/properties/{property.id}">
-                            <PropertyCard {property} fullWidth>
-                                <p class="text-ellipsis overflow-hidden text-nowrap" slot="more-info">
-                                    <strong>{property.pricePerNight} DKK</strong> night
-                                </p>
-                            </PropertyCard>
-                        </a>
-                    {/each}
-            </section>
-        {:else}
-            <section class="flex justify-center items-center">
+        {#if loading}
+            <section class="flex flex-col gap-2 justify-center items-center">
+                <div class="h-16 w-16 rounded-full border [border-width:7px] border-gray-300 border-t-rose-500 animate-spin" />
                 <p class="text-xl">
-                    No properties to list
+                    Loading data...
                 </p>
             </section>
+        {:else}
+            {#if properties.length}
+                <section class={[
+                    'w-full h-fit [display:grid] [grid-template-columns:repeat(1,minmax(0,1fr))] [grid-auto-rows:auto] [grid-auto-flow:row] gap-6',
+                    'sm:[grid-template-columns:repeat(2,minmax(0,1fr))]',
+                    '2xl:[grid-template-columns:repeat(3,minmax(0,1fr))]',
+                    '3xl:[grid-template-columns:repeat(4,minmax(0,1fr))]',
+                ].join(' ')}>
+                        {#each properties as property}
+                            <a class="h-fit" href="/properties/{property.id}">
+                                <PropertyCard {property} fullWidth>
+                                    <p class="text-ellipsis overflow-hidden text-nowrap" slot="more-info">
+                                        <strong>{property.pricePerNight} DKK</strong> night
+                                    </p>
+                                </PropertyCard>
+                            </a>
+                        {/each}
+                </section>
+            {:else}
+                <section class="flex justify-center items-center">
+                    <p class="text-xl">
+                        No properties to list
+                    </p>
+                </section>
+            {/if}
         {/if}
     </div>
     <div class="hidden w-2/5 h-[calc(100%-5rem)] absolute right-0 lg:block">
@@ -59,8 +82,9 @@
             center={[ lon, lat ]}
             {zoom}
             bind:map={map}
+            on:recentre={onMapMoveEnd}
         >
-            {#each $page.data.properties as property}
+            {#each properties as property}
                 <Marker
                     lat={property.lat}
                     lng={property.lon}
