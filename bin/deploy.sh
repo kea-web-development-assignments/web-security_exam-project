8c7e7188e8d89f59f9265c69f23dba013852917d
Haidar Almajedi <haidar@deranged.dk>
2024-12-08 03:30:17 +0100

    Update deploy script to use the production docker compose file and retroactively pull new images when they are available

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,25 +1,4 @@
 #!/bin/sh
 
 cd ~/development-environments_exam-project || exit 1;
-git fetch --all;
-
-SECONDS_SINCE_LAST_COMMIT=$(($(date +%s)-$(git log gitlab/main -1 --pretty='format:%cd' --date='unix')))
-if [ "$SECONDS_SINCE_LAST_COMMIT" -lt "30" ]; then #wait 30 seconds before deploying, so ci pipeline has a chance to run first
-    exit 0;
-fi;
-
-LAST_CI_STATUS=$(glab ci list -P 1);
-case "$LAST_CI_STATUS" in # exit if last ci pipeline did not succeed
-    *"(success)"*) : ;;
-    *) exit 0;;
-esac;
-
-DIFF=$(git diff gitlab/main --shortstat); # get short diff to check if main has changed
-if [ -n "$DIFF" ]; then
-    git pull gitlab main;
-    docker-compose down;
-    docker-compose build --no-cache;
-    docker-compose up -d;
-else
-    exit 0;
-fi;
+docker compose -f docker-compose.production.yml up -d --pull always
